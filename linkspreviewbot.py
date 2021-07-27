48a7ed5f8c8b961350ad17bf4d3edacb2d5b7afe
gwynbleidd415 <mankuagarwal7@gmail.com>
2021-07-27 19:15:51 +0530

    Bug fixes and improvements

--- linkspreviewbot.py
+++ linkspreviewbot.py
@@ -1,52 +1,63 @@
 import discord
-import re
 import os
+import re
 import requests
 from lxml import html
 from dotenv import load_dotenv
-# import nest_asyncio
 
+# import nest_asyncio
 
 # nest_asyncio.apply()
 
-load_dotenv('.env')
-token = os.getenv('BOT_TOKEN')
-pattern = re.compile(r'^https?://\S*\.\S*$')
+load_dotenv(".env")
+token = os.getenv("BOT_TOKEN")
+pattern = re.compile(r"^https?://\S*\.\S*$")
 
 client = discord.Client()
 
 
 def getTagValue(tree, tag):
     metaList = tree.xpath(
-        f'//meta[@*="{tag}" or @*="og:{tag}" or @*="twitter:{tag}"]/@content')
-    if(metaList):
+        f'//meta[@*="{tag}" or @*="og:{tag}" or @*="twitter:{tag}"]/@content'
+    )
+    if metaList:
         return metaList[-1]
     return None
 
 
 @client.event
 async def on_start():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if(pattern.search(message.content)):
+    if pattern.search(message.content):
         res = requests.get(message.content)
-        tree = html.fromstring(res.content).find('head')
-        thumbnail = getTagValue(tree, 'image')
-        if(not thumbnail):
-            thumbnail = getTagValue(tree, 'image:src')
-        embed = discord.Embed(title=getTagValue(tree, 'title'), url=message.content, description=getTagValue(
-            tree, 'description'), provider=getTagValue(tree, 'site_name'))
-        if(thumbnail):
+        tree = html.fromstring(res.content).find("head")
+        title, description = (
+            getTagValue(tree, "title"),
+            getTagValue(tree, "description"),
+        )
+        siteName = getTagValue(tree, "site")
+        thumbnail = getTagValue(tree, "image")
+        if not thumbnail:
+            thumbnail = getTagValue(tree, "image:src")
+        if not siteName:
+            siteName = getTagValue(tree, "site_name")
+        if description:
+            description = description[:119]
+        embed = discord.Embed(title=title, url=message.content, description=description)
+        if thumbnail:
             embed.set_thumbnail(url=thumbnail)
-        embed.set_footer(text=getTagValue(tree, 'site_name'))
+        if siteName:
+            embed.set_footer(text=siteName)
         # embed.set_author(name = getTagValue(tree, 'author'))
-        await message.channel.send(embed=embed)
-        await message.delete()
+        if title or description:
+            await message.channel.send(embed=embed)
+            await message.delete()
 
 
 client.run(token)
